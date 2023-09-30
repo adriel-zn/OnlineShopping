390824ed47f1f45db492618a878820643db0a049
Adriel Mariappen <adriel@geminisolution.co.za>
2023-10-01 01:55:57 +0200

    Update project references.

---
+++ OnlineShopping.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using OnlineShopping.Data;
+using OnlineShopping.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShopping.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
