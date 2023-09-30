b920504565990bd73f72715605fa0a451b247ff5
Adriel Mariappen <adriel@geminisolution.co.za>
2023-10-01 01:31:52 +0200

    Add enum folder and Product category model.

--- OnlineShopping.WebApi/Models/ProductModel.cs
+++ OnlineShopping.WebApi/Models/ProductModel.cs
@@ -5,7 +5,9 @@
         public string Title { get; set; }
         public string Description { get; set; } 
         public decimal Price { get; set; }
-        public int CategoryId { get; set; }
+        public int ProductCategoryId { get; set; }
         public byte[]? Image { get; set; }
+
+        public ProductCategoryModel ProductCategory { get; set; }
     }
 }
