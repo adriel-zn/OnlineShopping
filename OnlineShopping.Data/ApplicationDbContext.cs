c158c8e035f9105d9ab7f2f2694082d42450ac11
Adriel Mariappen <adriel@geminisolution.co.za>
2023-10-01 01:50:49 +0200

    Add data layer to communicate with backend.

---
+++ OnlineShopping.Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShopping.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShopping.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        
+
+    }
+}
---
+++ OnlineShopping.Data/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShopping.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int ProductCategoryId { get; set; }
+        public byte[]? Image { get; set; }
+    }
+}
