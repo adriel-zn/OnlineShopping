62b31347a979938de2148df5fd45c09d29d75148
Adriel Mariappen <adriel@geminisolution.co.za>
2023-10-02 02:33:10 +0200

    Add product category repo and interface

---
+++ OnlineShopping.Infrastructure/Interfaces/IProductCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using OnlineShopping.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShopping.Infrastructure.Interfaces
+{
+    public interface IProductCategoryRepository
+    {
+        Task<IList<ProductCategory>> GetAll();
+        Task<ProductCategory> GetById(int id);
+        Task Create(ProductCategory model);
+        Task Update(ProductCategory model);
+        Task Delete(int id);
+    }
+}
